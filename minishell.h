755822987e2aeac4f1b7460ef5b9e44e056185df
Anthony Kinzelin <akinzeli@student.42luxembourg.lu>
2024-05-22 13:28:01 +0200

    minishell.h

---
+++ minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akinzeli <akinzeli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 13:25:39 by akinzeli          #+#    #+#             */
+/*   Updated: 2024/05/22 13:27:36 by akinzeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <dirent.h>
+
+#endif
