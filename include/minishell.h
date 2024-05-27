658df862054d40114b0c1a29caf0924dae6c6408
brhma54 <137801363+brhma54@users.noreply.github.com>
2024-05-27 16:23:31 +0200

    clean

--- include/minishell.h
+++ include/minishell.h
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -21,44 +22,5 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <dirent.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-typedef enum e_tokens
-{
-	NUMBER,
-	STRING,
-	SYMBOL,
-	SPC,
-	PIPE,
-	ADD,
-	SOUSTRACT,
-	MULTI,
-	DOLLAR,
-	SLASH,
-	BACKSLASH,
-	COMA,
-	DOT,
-	DASH,
-	QUOTE,
-	DOUBLEQUOTE,
-	UNDERSCORE,
-	EQUAL,
-	SEMICOLON,
-	LEFT_ARROW,
-	RIGHT_ARROW,
-}		t_tokens;
-
-typedef struct s_list
-{
-	char			*content;
-	t_tokens	token;
-	struct s_list	*next;
-}		t_list;
-
-int	ft_isalpha(int c);
-unsigned int	ft_strlcpy(char *dest, char const *src, unsigned int size);
-int	ft_isdigit(int c);
-int	ft_strncmp(char const *str1, char const *str2, unsigned int n);
 
 #endif
